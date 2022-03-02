5bf2476aed77596f8620a324b3e2cb4b79f61dae
Jay Michalek <39870122+jaymichalek@users.noreply.github.com>
2022-03-02 17:08:18 -0600

    Delete main.py

--- main.py
+++
@@ -1,7 +0,0 @@
-from PIL import Image, ImageFilter
-
-img = Image.open('astro.jpg')
-# Keeps aspect ratio as close to specified size
-img.thumbnail((400, 200))
-img.save('thumbnail.jpg')
-print(img.size)
