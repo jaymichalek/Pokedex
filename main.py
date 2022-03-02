98fe50ef42157898dc862fde197bd7e88702abe9
hashtagd3v <jaycoinminer@gmail.com>
2022-03-02 15:51:12 -0600

    Crop images

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from PIL import Image, ImageFilter
 
 img = Image.open('image-playground/charmander.jpg')
 filtered_img = img.convert('L')
-resize = filtered_img.resize((300, 300))
-resize.save("grayCharmander.png", 'png')
+box = (100, 100, 400, 400)
+region = filtered_img.crop(box)
+region.save("grayCharmander.png", 'png')
 # filtered_img.show()
