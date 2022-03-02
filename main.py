557d90031d5b456dddea702a6267193618287f04
hashtagd3v <jaycoinminer@gmail.com>
2022-03-02 16:02:15 -0600

    Resize images while keeping aspect ratio

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 from PIL import Image, ImageFilter
 
-img = Image.open('image-playground/charmander.jpg')
-filtered_img = img.convert('L')
-box = (100, 100, 400, 400)
-region = filtered_img.crop(box)
-region.save("grayCharmander.png", 'png')
-# filtered_img.show()
+img = Image.open('image-playground/astro.jpg')
+# Keeps aspect ratio as close to specified size
+img.thumbnail((400, 200))
+img.save('thumbnail.jpg')
+print(img.size)
