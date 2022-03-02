4302d6880cf4ff35d5069389d0ca5b239252034b
hashtagd3v <jaycoinminer@gmail.com>
2022-03-02 15:46:17 -0600

    Resize images

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ from PIL import Image, ImageFilter
 
 img = Image.open('image-playground/charmander.jpg')
 filtered_img = img.convert('L')
-crooked = filtered_img.rotate(180)
-crooked.save("grayCharmander.png", 'png')
+resize = filtered_img.resize((300, 300))
+resize.save("grayCharmander.png", 'png')
 # filtered_img.show()
